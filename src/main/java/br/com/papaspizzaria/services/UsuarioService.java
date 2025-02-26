593eab46751bf460289c1e35f8a8423f4c8b87dd
EternalGerms <matheusesp.profissional@gmail.com>
2025-02-26 16:52:14 -0300

    sistema de email incompleto, por enquanto só envia mesmo

--- src/main/java/br/com/papaspizzaria/services/UsuarioService.java
+++ src/main/java/br/com/papaspizzaria/services/UsuarioService.java
@@ -13,53 +13,56 @@ import br.com.papaspizzaria.repositories.UsuarioRepository;
 
 @Service
 public class UsuarioService {
-	
+
 	@Autowired
 	private PasswordEncoder passwordEncoder;
 
 	@Autowired
-    private UsuarioRepository usuarioRepository;
-    
-    public UsuarioService(UsuarioRepository usuarioRepository) {
-    	this.usuarioRepository = usuarioRepository;
-    }
+	private UsuarioRepository usuarioRepository;
 
-    public List<UsuarioDTO> listarUsuarios() {
-        List<Usuario> usuarios = usuarioRepository.findAll();
-        return usuarios.stream().map(UsuarioDTO::new).toList();
-    }
+	@Autowired
+	private EmailService emailService;
 
-    public void salvarUsuario(UsuarioDTO usuario) {
-    	Usuario usuarioEntity = new Usuario(usuario);
-    	usuarioEntity.setSenha(passwordEncoder.encode(usuario.getSenha()));
-    	usuarioRepository.save(usuarioEntity);
-    }
-    
-    public void registrarNovoUsuario(UsuarioDTO usuario) {
-    	Usuario usuarioEntity = new Usuario(usuario);
-    	usuarioEntity.setSenha(passwordEncoder.encode(usuario.getSenha()));
-    	usuarioEntity.setSituacao(TipoSituacaoUsuario.PENDENTE);
-    	usuarioEntity.setId(null);
-    	usuarioRepository.save(usuarioEntity);
-    	
-    	//TODO Enviar um email para verificar a conta
-    	
-    	
-    }
-    
-    public UsuarioDTO alterarUsuario(UsuarioDTO usuario) {
-    	Usuario usuarioEntity = new Usuario(usuario);
-    	usuarioEntity.setSenha(passwordEncoder.encode(usuario.getSenha()));
-    	return new UsuarioDTO(usuarioRepository.save(usuarioEntity));
-    }
+	public UsuarioService(UsuarioRepository usuarioRepository) {
+		this.usuarioRepository = usuarioRepository;
+	}
 
-    public void excluirUsuario(Long id) {
-        Usuario usuario = usuarioRepository.findById(id).get();
-        usuarioRepository.delete(usuario);
-    }
-    
-    public UsuarioDTO buscarUsuario(Long id) {
-    	return new UsuarioDTO(usuarioRepository.findById(id).get());
-    }
-    
+	public List<UsuarioDTO> listarUsuarios() {
+		List<Usuario> usuarios = usuarioRepository.findAll();
+		return usuarios.stream().map(UsuarioDTO::new).toList();
+	}
+
+	public void salvarUsuario(UsuarioDTO usuario) {
+		Usuario usuarioEntity = new Usuario(usuario);
+		usuarioEntity.setSenha(passwordEncoder.encode(usuario.getSenha()));
+		usuarioRepository.save(usuarioEntity);
+	}
+
+	public void registrarNovoUsuario(UsuarioDTO usuario) {
+		Usuario usuarioEntity = new Usuario(usuario);
+		usuarioEntity.setSenha(passwordEncoder.encode(usuario.getSenha()));
+		usuarioEntity.setSituacao(TipoSituacaoUsuario.PENDENTE);
+		usuarioEntity.setId(null);
+		usuarioRepository.save(usuarioEntity);
+
+		emailService.enviarEmailTexto(usuario.getEmail(), "Novo usuário cadastrado",
+				"Você está recebendo um email de cadastro!");
+
+	}
+
+	public UsuarioDTO alterarUsuario(UsuarioDTO usuario) {
+		Usuario usuarioEntity = new Usuario(usuario);
+		usuarioEntity.setSenha(passwordEncoder.encode(usuario.getSenha()));
+		return new UsuarioDTO(usuarioRepository.save(usuarioEntity));
+	}
+
+	public void excluirUsuario(Long id) {
+		Usuario usuario = usuarioRepository.findById(id).get();
+		usuarioRepository.delete(usuario);
+	}
+
+	public UsuarioDTO buscarUsuario(Long id) {
+		return new UsuarioDTO(usuarioRepository.findById(id).get());
+	}
+
 }
